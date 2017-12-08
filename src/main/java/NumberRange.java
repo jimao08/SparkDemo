5ccab33ff1ebb7450e347f88e66141b6e8ad696d
linkang <jimao08@sina.com>
2017-12-08 22:43:41 +0800

    first demo

--- src/main/java/NumberRange.java
+++ src/main/java/NumberRange.java
@@ -10,14 +10,6 @@ public class NumberRange {
         this.end = end;
     }
 
-    public int getStart() {
-        return start;
-    }
-
-    public int getEnd() {
-        return end;
-    }
-
     @Override
     public String toString() {
         return "NumberRange{" +
@@ -28,41 +20,30 @@ public class NumberRange {
 
     public static boolean check(int number, List<NumberRange> ranges) {
         boolean pass = false;
-        for (NumberRange range : ranges) {
-            if (range.start == range.end) {
-                pass = number == range.start;
-            }
-            if (pass) {
-                break;
-            }
 
+        for (int i = 0; i < ranges.size() && !pass; i++) {
+            NumberRange range = ranges.get(i);
             if (range.end == -1) {
-                pass = number > range.start;
-            }
-
-            if (pass) {
-                break;
-            }
-
-            if (range.start < range.getEnd()) {
+                pass = number >= range.start;
+            } else if (range.start <= range.end) {
                 pass = number <= range.end && number >= range.start;
-            }
-            if (pass) {
-                break;
             }
         }
         return pass;
     }
 
     public static List<NumberRange> parse(String expression) {
-
         List<NumberRange> ranges = new ArrayList<>();
 
         for (String str : expression.split("\\|")) {
+            if (!str.contains("[") || !str.contains("]")) {
+                return null;
+            }
+
             String[] split = str.split(",");
 
             if (split.length != 2) {
-                continue;
+                return null;
             }
 
             String s0 = split[0];
@@ -73,9 +54,9 @@ public class NumberRange {
             int index2 = s1.indexOf("]");
             int num2 = Integer.valueOf(s1.substring(0, index2).trim());
 
-
-            if (num2 != -1 && num1 > num2) {
-                continue;
+            if ((num2 != -1 && (num1 > num2 || num2 <= 0))
+                    || num1 <= 0) {
+                return null;
             }
 
             NumberRange range = new NumberRange(num1, num2);
--- src/main/java/RangeTest.java
+++ src/main/java/RangeTest.java
@@ -1,16 +1,24 @@
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class RangeTest {
 
 
     public static void main(String[] args) {
-        String test = "[1,3] | [4,6  ] | [30,-1]";
+        String test = "[3,40]|";
 
         List<NumberRange> ranges = NumberRange.parse(test);
 
         System.out.println(ranges);
 
-        System.out.println(NumberRange.check(100, ranges));
+        if (ranges == null || ranges.isEmpty()) {
+            System.out.println("wrong params");
+            return;
+        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println("i=" +i + ">>" + NumberRange.check(i, ranges));
+        }
+
+
     }
 }
